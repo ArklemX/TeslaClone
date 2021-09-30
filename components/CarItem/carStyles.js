eeec6811f605e0cd193c7dfdb5bb2b43df339f7f
ArklemX <avaikamarcel@gmail.com>
2021-09-30 04:34:16 +0100

    Header and FlatList components

--- components/CarItem/carStyles.js
+++ components/CarItem/carStyles.js
@@ -1,10 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 const carStyles = StyleSheet.create({
     carContainer: {
         // backgroundColor: '#F0A',
-        width: '100%',
-        height: '100%'
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height
     },
     titles: {
         marginTop : '30%',
